79f3e343726de676e45987507c2b5bd8081b2ba3
user <user@users-MacBook-Pro.local>
2023-06-27 16:08:43 +0700

    Set up database with one table, invoice

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+class Invoice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Invoice {self.id}>'
 
 if __name__ == '__main__':
     app.run(debug=True)
