3bb4ab23197785cf04f12a0a8948ca2e67f12ec4
user <user@users-MacBook-Pro.local>
2023-06-27 16:29:21 +0700

    Added routes to create and read invoices

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 db = SQLAlchemy(app)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+@app.route("/invoice", methods=["POST"])
+def create_invoice():
+    data = request.get_json()
+    new_invoice = Invoice(date=datetime.datetime.now(), amount=data["amount"])
+    db.session.add(new_invoice)
+    db.session.commit()
+    return {"id": new_invoice.id}, 201
+
+
+@app.route("/invoice/<int:id>", methods=["GET"])
+def get_invoice(id):
+    invoice = Invoice.query.get(id)
+    if invoice is None:
+        return {"error": "not found"}, 404
+    return {"id": invoice.id, "date": str(invoice.date), "amount": invoice.amount}
+
+
+@app.route("/invoices", methods=["GET"])
+def get_invoices():
+    all_invoices = Invoice.query.all()
+    return {
+        "invoices": [
+            {"id": invoice.id, "date": str(invoice.date), "amount": invoice.amount}
+            for invoice in all_invoices
+        ]
+    }
+
 
 class Invoice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +47,8 @@ class Invoice(db.Model):
     amount = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f'<Invoice {self.id}>'
+        return f"<Invoice {self.id}>"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
